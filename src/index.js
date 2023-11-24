25b6c05158c3761f44c8909dc3417c77294557d3
raviteja-08 <128289383+raviteja-08@users.noreply.github.com>
2023-11-24 23:38:41 +0530

    express configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 // require('dotenv').config({path:"./env"})
 import dotenv from "dotenv";
 import mongoConnect from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path:'./env'
 })
-mongoConnect();
+const port = process.env.PORT||8000;
+mongoConnect()
+.then((res)=>{
+    app.listen(port,()=>{
+        console.log(`server is connected on ${port}`);
+    })
+})
+.catch(error=>{
+    console.log("not connected to db!!\n",error);
+})
 
